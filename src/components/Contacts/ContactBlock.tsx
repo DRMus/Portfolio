f7acd22a7ac2ad246cfe7e8d2384f9741e21b999
Muslimov_DR <muslimov@muslimov>
2023-06-05 15:05:30 +0400

    fix onClick on ContactBlock

--- src/components/Contacts/ContactBlock.tsx
+++ src/components/Contacts/ContactBlock.tsx
@@ -41,11 +41,11 @@ const ContactBlock: FC<ContactBlockProps> = ({ children, ...props }) => {
       copyViewTimeout.current[1] = setTimeout(() => {
         setIsCopiedViewVisible(false);
         copyViewTimeout.current[1] = -1;
-      }, 350);
+      }, 250);
     }, 1050);
   };
 
-  const onClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+  const onClick = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
     if (props.type === "clipboard" && props.title) {
       event.preventDefault();
       copyToClipBoard(props.title);
@@ -54,27 +54,32 @@ const ContactBlock: FC<ContactBlockProps> = ({ children, ...props }) => {
 
   const componentClassNames = {
     a: classNames("flex select-text gap-4 items-center h-full w-full px-4 py-2", {
-      "hideElement opacity-0": isAnimationRising && isCopiedViewVisible && props.type === "clipboard",
-      "showElement opacity-1": !isAnimationRising && isCopiedViewVisible && props.type === "clipboard",
+      "hideElement opacity-0":
+        isAnimationRising && isCopiedViewVisible && props.type === "clipboard",
+      "showElement opacity-1":
+        !isAnimationRising && isCopiedViewVisible && props.type === "clipboard",
     }),
     icon: classNames("transition-colors duration-[350ms] text-gray-500", props.iconClassName),
     paragraph: classNames(
       "transition-colors duration-[350ms] text-gray-500 grow text-center !font-bold",
       props.iconClassName
     ),
-    copiedHeader: classNames("absolute top-0 left-0 right-0 bottom-0 flex grid place-items-center select-none", {
-      "hideElement opacity-0": !isAnimationRising && props.type === "clipboard",
-      "showElement opacity-1": isAnimationRising && props.type === "clipboard",
-    })
+    copiedHeader: classNames(
+      "absolute top-0 left-0 right-0 bottom-0 flex grid place-items-center select-none",
+      {
+        "hideElement opacity-0": !isAnimationRising && props.type === "clipboard",
+        "showElement opacity-1": isAnimationRising && props.type === "clipboard",
+      }
+    ),
   };
   return (
-    <BluredBlock className={classNames("relative", props.className)} hoverSize="1x" hoverLight="3x">
-      <a
-        href={props.href || "#"}
-        className={componentClassNames.a}
-        target="_blank"
-        onClick={onClick}
-      >
+    <BluredBlock
+      className={classNames("relative", props.className)}
+      hoverSize="1x"
+      hoverLight="3x"
+      onClick={onClick}
+    >
+      <a href={props.href || "#"} className={componentClassNames.a} target="_blank">
         {children ? (
           children
         ) : (
@@ -101,4 +106,4 @@ const ContactBlock: FC<ContactBlockProps> = ({ children, ...props }) => {
   );
 };
 
-export default ContactBlock
+export default ContactBlock;
