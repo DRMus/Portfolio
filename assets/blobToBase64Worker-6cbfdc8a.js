c611f1c8167698bd4338398fa4ab5e678032f126
Damir Muslimov <78694252+DRMus@users.noreply.github.com>
2023-06-21 10:57:52 +0400

    Updates

--- assets/blobToBase64Worker-6cbfdc8a.js
+++
@@ -1,1 +0,0 @@
-(function(){"use strict";self.addEventListener("message",e=>{s(e.data)});function s(e){var a=new FileReader;a.readAsDataURL(e),a.onloadend=function(){var n=a.result;self.postMessage(n)}}})();
