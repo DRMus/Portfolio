d2dcdfe548f4924016bd8760a527e18f6d19c191
DRMus <78694252+DRMus@users.noreply.github.com>
2023-06-23 23:42:44 +0400

    width full for footer menu on mobile devices

--- src/components/MainFragments/Footer/FooterMenu.tsx
+++ src/components/MainFragments/Footer/FooterMenu.tsx
@@ -5,17 +5,21 @@ import classNames from "classnames";
 import HoverIsland from "../../templates/HoverIsland";
 
 const FooterMenu = () => {
+  const componentClassNames = {
+    footer: classNames("fixed bottom-0 z-30", "sm:left-1/2 sm:-translate-x-1/2 sm:mb-6", "max-sm:w-full max-sm:flex max-sm:items-center"),
+    island: classNames(
+      "w-fit flex gap-x-6 my-0 px-6 py-3 mx-auto items-center rounded-lg text-center",
+      "max-md:px-4",
+      "max-sm:px-4 max-sm:py-4 max-sm:w-full max-sm:justify-around max-sm:rounded-b-none max-sm:rounded-t-xl",
+      styles.showFooter
+    ),
+  };
   return (
-    <footer className="fixed bottom-0 left-1/2 -translate-x-1/2 mb-6 z-30">
-      <HoverIsland
-        className={classNames(
-          "w-fit flex gap-x-6 my-0 px-6 py-3 mx-auto items-center rounded-lg text-center",
-          "max-md:px-4",
-          "max-sm:px-4 max-sm:py-2",
-          styles.showFooter
-        )}
-      >
-        <FooterMenuItem to="about" className="max-md:whitespace-nowrap">About Me</FooterMenuItem>
+    <footer className={componentClassNames.footer}>
+      <HoverIsland className={componentClassNames.island}>
+        <FooterMenuItem to="about" className="max-md:whitespace-nowrap">
+          About Me
+        </FooterMenuItem>
         <FooterMenuItem to="projects">Projects</FooterMenuItem>
         <FooterMenuItem to="contacts">Contacts</FooterMenuItem>
       </HoverIsland>
